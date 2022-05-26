3a301855c3b5d988eb1d1a611bda0ac0d1be1106
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-25 21:30:16 -0700

    commit tree 62cb28bdb17279f4ad2a651aafff1dcebcd05a06

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+//implement a function called saturdayFun
+
+function saturdayFun(activity = 'roller-skate') {
+   return (`This Saturday, I want to ${activity}!`)
+} 
+saturdayFun();
+saturdayFun('bathe my dog');
+
+//implement a function called mondayWork
+
+function mondayWork(workAct = 'go to the office') {
+
+    return (`This Monday, I will ${workAct}.`)
+
+}
+
+mondayWork();
+
+
+//implement a function called wrapAdjective
+
+function wrapAdjective(sign = '*'){
+    return function (name='special') {
+        return `You are ${sign}${name}${sign}!`
+    }
+}
